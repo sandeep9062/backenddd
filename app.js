e9de2468e49462f7643d963a7d854bd3d1484873
Sandeep Saini <sandeepsaini157219@gmail.com>
2025-10-18 18:27:04 +0530

    checkcors

--- app.js
+++ app.js
@@ -44,7 +44,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://frontenddd-sepia.vercel.app",
+    ],
     credentials: true,
   })
 );
